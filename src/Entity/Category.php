57ac9c6ec0a53cdb2576497d527603ba5c108d96
Mariia Diachenko <M_sha@ukr.net>
2019-05-27 16:30:09 +0000

    validation for category name added

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
@@ -20,6 +21,9 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Regex("/^[\p{L}_\d ]+$/")
+     * @Assert\Length(min=1, max=254)
      */
     private $name;
 
