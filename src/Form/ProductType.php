f63858a32d00d2dcc2186b348169a42bf75a1005
Mariia Diachenko <M_sha@ukr.net>
2019-05-27 16:07:38 +0000

    crud for shop product and category added

---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('img')
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('isRecommended')
+            ->add('isNew')
+            ->add('category')
+            ->add('shop')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
