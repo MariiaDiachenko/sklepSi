ca0dcc264c597a7eb219d7217349a35f3078e979
Mariia Diachenko <M_sha@ukr.net>
2019-05-27 15:43:19 +0000

    Previous fixtures added

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Category;
+
+class CategoryFixtures extends AbstractBaseFixtures
+{
+    public function loadData(ObjectManager $manager): void
+    {
+      for ($i=0; $i < 10; $i++) {
+        $category = new Category();
+        $category->setName($this->faker->name());
+        $this->manager->persist($category);
+      }
+      $this->manager->flush();
+    }
+}
